32e3cca31d034baf80a51873c4b2ddef3e164e14
Florian Bader <florian.bader@aitgmbh.de>
2021-06-05 13:47:31 +0200

    testing

--- src/Infrastructure/Scripts/azure-post-deployment.ps1
+++ src/Infrastructure/Scripts/azure-post-deployment.ps1
@@ -9,28 +9,33 @@ $pulumiOutputs = (Get-Content "$rootDirectory/src/Infrastructure/outputs.json" |
 Install-Module -Name SqlServer -Force -Scope CurrentUser
 
 $ip = (Invoke-RestMethod http://ipinfo.io/json | Select-Object -exp ip)
+Write-Host "Creating Azure SQL firewall rule for ip $ip"
 az sql server firewall-rule create -g "webapi-$environment-euw-rg" -s "webapi-$environment-euw-sqls" -n "BuildAgent" --start-ip-address $ip --end-ip-address $ip
 
-$token = (Get-AzAccessToken -ResourceUrl https://database.windows.net).Token
+try {
+    $token = (Get-AzAccessToken -ResourceUrl https://database.windows.net).Token
 
-$query = "IF NOT EXISTS(SELECT 1 FROM sys.database_principals WHERE name ='webapi-$environment-euw-app')
-    BEGIN
-        CREATE USER [webapi-$environment-euw-app] WITH DEFAULT_SCHEMA=[dbo], SID = $sid, TYPE = E;
-    END
+    Invoke-SqlCmd -ServerInstance "webapi-$environment-euw-sqls" -Database "webapi-$environment-euw-sqld" -AccessToken "$token" -Query "SELECT 1"
 
-    IF IS_ROLEMEMBER('db_datareader','webapi-$environment-euw-app') = 0
-    BEGIN
-        ALTER ROLE db_datareader ADD MEMBER [webapi-$environment-euw-app]
-    END
+    $query = "IF NOT EXISTS(SELECT 1 FROM sys.database_principals WHERE name ='webapi-$environment-euw-app')
+        BEGIN
+            CREATE USER [webapi-$environment-euw-app] WITH DEFAULT_SCHEMA=[dbo] FROM EXTERNAL PROVIDER;
+        END
 
-    IF IS_ROLEMEMBER('db_datawriter','webapi-$environment-euw-app') = 0
-    BEGIN
-        ALTER ROLE db_datawriter ADD MEMBER [webapi-$environment-euw-app]
-    END;"
+        IF IS_ROLEMEMBER('db_datareader','webapi-$environment-euw-app') = 0
+        BEGIN
+            ALTER ROLE db_datareader ADD MEMBER [webapi-$environment-euw-app]
+        END
 
-Invoke-SqlCmd -ServerInstance "webapi-$environment-euw-sqls" `
-    -Database "webapi-$environment-euw-sqld" `
-    -AccessToken "$token" `
-    -Query "$query"
+        IF IS_ROLEMEMBER('db_datawriter','webapi-$environment-euw-app') = 0
+        BEGIN
+            ALTER ROLE db_datawriter ADD MEMBER [webapi-$environment-euw-app]
+        END;"
 
-az sql server firewall-rule delete -g "webapi-$environment-euw-rg" -s "webapi-$environment-euw-sqls" -n "BuildAgent"
+    Write-Host "Adding permission for apps in Azure SQL database"
+    Invoke-SqlCmd -ServerInstance "webapi-$environment-euw-sqls" -Database "webapi-$environment-euw-sqld" -AccessToken "$token" -Query "$query"
+}
+finally {
+    Write-Host "Removing Azure SQL firewall rule"
+    az sql server firewall-rule delete -g "webapi-$environment-euw-rg" -s "webapi-$environment-euw-sqls" -n "BuildAgent"
+}
