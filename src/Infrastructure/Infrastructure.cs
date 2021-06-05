ae95b2075ebc6aded95dc33a9a7c92f84e52ca85
Florian Bader <florian.bader@aitgmbh.de>
2021-06-05 12:38:27 +0200

    testing

--- src/Infrastructure/Infrastructure.cs
+++ src/Infrastructure/Infrastructure.cs
@@ -13,7 +13,7 @@ namespace Infrastructure
 
             var resourceGroup = new ResourceGroupResource();
 
-            var keyVault = new KeyVaultResource(resourceGroup);
+            var keyVault = new KeyVaultResource(resourceGroup, currentUserObjectId);
 
             var appServicePlan = new AppServicePlanResource(resourceGroup);
             appServicePlan.Build();
